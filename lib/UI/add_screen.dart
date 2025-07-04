6c21cc3780930c21d61e83c14b21f37fafb7d98c
boursambath <bkbath2@gmail.com>
2025-07-04 18:06:39 +0700

    i finish

---
+++ lib/UI/add_screen.dart
@@ -0,0 +1,117 @@
+import 'package:flutter/material.dart';
+class AddScreen extends StatefulWidget {
+  const AddScreen({super.key});
+  @override
+ State<AddScreen> createState() => _AddScreenState();
+}
+
+class _AddScreenState extends State<AddScreen> {
+  List<String> films = [
+    'Mulan',
+    'Justice league',
+    'Before sunrise',
+    'It end with us',
+    'Kungfu',
+    'Des',
+    'Minion',
+    'Inside',
+    'Dead',
+    'Planet of',
+    'Alien',
+    'The wild',
+    'God',
+    'Saltburn',
+    'Dune 2',
+    'La la',
+    'How to',
+  ];
+  List<String> filteredFilms = [];
+
+  @override
+  void initState() {
+    super.initState();
+    filteredFilms = films;
+  }
+
+  void _filterFilms(String query) {
+    setState(() {
+      filteredFilms = films
+          .where((film) => film.toLowerCase().contains(query.toLowerCase()))
+          .toList();
+    });
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blueGrey[900],
+      body: SafeArea(
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 10),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  GestureDetector(
+                    onTap: () => Navigator.pop(context),
+                    child: Text(
+                      'Cancel',
+                      style: TextStyle(color: Colors.white, fontSize: 16),
+                    ),
+                  ),
+                  Text(
+                    'Add a Film',
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 18,
+                        color: Colors.white),
+                  ),
+                  CircleAvatar(
+                    radius: 18,
+                    backgroundImage: AssetImage('assets/patrick.png'), 
+                  ),
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16),
+              child: TextField(
+                onChanged: _filterFilms,
+                style: TextStyle(color: Colors.white),
+                decoration: InputDecoration(
+                  hintText: 'Name of film',
+                  hintStyle: TextStyle(color: Colors.white54),
+                  prefixIcon: Icon(Icons.search, color: Colors.white54),
+                  filled: true,
+                  fillColor: Colors.blueGrey[800],
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(12),
+                    borderSide: BorderSide.none,
+                  ),
+                  contentPadding: EdgeInsets.symmetric(vertical: 0),
+                ),
+              ),
+            ),
+            SizedBox(height: 10),
+            Expanded(
+              child: ListView.builder(
+                itemCount: filteredFilms.length,
+                itemBuilder: (context, index) {
+                  return ListTile(
+                    title: Text(
+                      filteredFilms[index],
+                      style: TextStyle(color: Colors.white),
+                    ),
+                    onTap: () {
+                      print('Selected: ${filteredFilms[index]}');
+                    },
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
